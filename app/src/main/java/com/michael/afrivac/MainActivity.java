c3ed0261f1cb5c847fc9299fc40ba67f83a1471b
Umeh Michael <55640053+Michaelzy27@users.noreply.github.com>
2020-06-19 22:47:44 +0100

    Revert "Modified the drawer Layout"

--- app/src/main/java/com/michael/afrivac/MainActivity.java
+++ app/src/main/java/com/michael/afrivac/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-      //  setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
