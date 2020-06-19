b3b7b4ffeab14b79df74f3539375ea1e7e3940d5
Qozeem Odeniran <muhdqozeem@gmail.com>
2020-06-19 04:45:46 +0100

    added currency convert

--- app/src/main/java/com/michael/afrivac/MainActivity.java
+++ app/src/main/java/com/michael/afrivac/MainActivity.java
@@ -3,9 +3,33 @@ package com.michael.afrivac;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public void convertCurrency(View view){
+        EditText editText = (EditText) findViewById(R.id.edtText);
+        int currency = Integer.parseInt(editText.getText().toString());
+//        double dollar;
+        double result;
 
+        switch (currency){
+            case 1: //Dollar to Naira
+                double dollar = 360;
+                result = currency * dollar;
+                break;
+            case 2: //Pounds to Naira
+                double pounds = 450;
+                result = currency * pounds;
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + currency);
+        }
+
+        Toast.makeText(MainActivity.this, Double.toString(result), Toast.LENGTH_LONG).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
