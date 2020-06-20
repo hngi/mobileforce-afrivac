6ed30cd25305bb72314609bef8a0734b3b71de5a
Bolaji <51162350+Bilijin@users.noreply.github.com>
2020-06-20 06:22:51 +0100

    Update BookingActivity.java

--- app/src/main/java/com/michael/afrivac/BookingActivity.java
+++ app/src/main/java/com/michael/afrivac/BookingActivity.java
@@ -2,23 +2,12 @@ package com.michael.afrivac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
-
-import java.util.Calendar;
 
 public class BookingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        final EditText editText;
-        final EditText editText2;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
     }
