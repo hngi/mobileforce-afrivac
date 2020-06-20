64d9bded25a81db7a955d068237ed6197d36af54
Odinakachi Nnuforole <53151644+Cuteod@users.noreply.github.com>
2020-06-20 23:40:48 +0100

    SignIn with firebase Auth

--- app/src/main/java/com/michael/afrivac/LoginActivity.java
+++ app/src/main/java/com/michael/afrivac/LoginActivity.java
@@ -1,44 +1,89 @@
 package com.michael.afrivac;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email, password;
+    private String Email, Password;
     private TextView sign_in;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         email = findViewById(R.id.signin_email);
         password = findViewById(R.id.signin_password);
         sign_in = findViewById(R.id.singin_into_account);
-        final String Email = email.getText().toString().trim();
-        final String Password = password.getText().toString().trim();
+
 
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Email.length() == 0 && Password.length() == 0) {
-                    if (Email.equals("admins") && Password.equals("admins")) {
-                        Toast.makeText(getApplicationContext(), "Redirecting to...", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Wrong log in details, please try again", Toast.LENGTH_LONG).show();
-                    }
-                }else {
-                    Toast.makeText(getApplicationContext(),"Email/Password must not be empty", Toast.LENGTH_LONG).show();
-                }
+                 Email = email.getText().toString().trim();
+                 Password = password.getText().toString().trim();
 
+                if (TextUtils.isEmpty(Email)){
+                    email.setError("enter your email");
+                    return;
+
+                }if (TextUtils.isEmpty(Password)){
+                    password.setError("enter password");
+                }else {
+                    checkNetwork();
+                }
             }
 
 
         });
+    }
+    private void checkNetwork(){
+        if (isNetWorkAvailable()){
+            mAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Log.d("login", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        startActivity(new Intent(LoginActivity.this, OnboardscreenActivity.class));
+                    }else {
+                        Log.w("sign in", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed...", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+
+        }else {
+            Toast.makeText(getApplicationContext(), "network error", Toast.LENGTH_LONG).show();
+        }
+    }
+    private boolean isNetWorkAvailable(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
