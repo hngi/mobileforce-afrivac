87fc2ff913430c1a08014ab3a40457fc4d89e564
Faith Korir <faithckorir@gmail.com>
2020-06-22 00:31:29 +0300

    added support page

--- app/src/main/java/com/michael/afrivac/Fragments/SupportFragment.java
+++ app/src/main/java/com/michael/afrivac/Fragments/SupportFragment.java
@@ -24,7 +24,7 @@ public class SupportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home2, container, false);
+        return inflater.inflate(R.layout.support_fragment, container, false);
     }
 
 
