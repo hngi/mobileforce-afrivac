a57d70f0a626a7e0a49503a562365c10394ff4fc
pecpaker <umehpeculiar20@gmail.com>
2020-06-21 11:58:26 +0100

    feat: Contact Us xml

---
+++ app/src/main/java/com/michael/afrivac/view/ContactUsActivity.java
@@ -0,0 +1,16 @@
+package com.michael.afrivac.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.michael.afrivac.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
