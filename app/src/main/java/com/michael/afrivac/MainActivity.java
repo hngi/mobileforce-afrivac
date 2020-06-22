38a6ff724f9a02dbbea7719a3887d7b1c0a989b8
Seyi <seyipaye@gmail.com>
2020-06-22 09:54:27 +0100

    feat: Improved navigation support for Popular places
    Improv the back button beahvour to accomodat popular places search

--- app/src/main/java/com/michael/afrivac/MainActivity.java
+++ app/src/main/java/com/michael/afrivac/MainActivity.java
@@ -55,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send,
+                R.id.nav_popular_destination)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
