ce5bea62905e21f2718cac80ad1f3a34bbc9b595
Chris-Eminence <emichris380@gmail.com>
2020-06-22 14:24:52 -0700

    google signUp feature

--- app/src/main/java/com/michael/afrivac/LoginActivity.java
+++ app/src/main/java/com/michael/afrivac/LoginActivity.java
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int RC_SIGN_IN = 0;
+    GoogleSignInClient mGoogleSignInClient;
 
     private EditText email, password;
     private TextView sign_in, signUp, forgotPassword;
@@ -17,12 +29,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        //initializing Google Signup features
+//        signInButton = findViewById(R.id.signin_with_google);
         email = findViewById(R.id.signin_email);
         password = findViewById(R.id.signin_password);
         sign_in = findViewById(R.id.singin_into_account);
         signUp = findViewById(R.id.singin_goto_signup);
         forgotPassword = findViewById(R.id.signin_forgot_password);
+
+
+
         final String Email = email.getText().toString().trim();
         final String Password = password.getText().toString().trim();
 
@@ -49,5 +65,70 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        //Configure sign in to request the user ID, Email Adress and basic
+        //profile ID, and Basic profile are included in the DEFAULT_SIGN_IN
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        //Build a GoogleSignInClient with options specified by gso
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.signin_with_google).setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signin_with_google:
+                signIn();
+                break;
+            // ...
+        }
+
+    }
+
+    private void signIn() {
+        Intent googleSignInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(googleSignInIntent, RC_SIGN_IN);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Google Sign In error", "signInResult:failed code="+ e.getStatusCode());
+            Toast.makeText(LoginActivity.this, "FAILED", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onStart(){
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+            if (account != null){
+                startActivity(new Intent (LoginActivity.this, MainActivity.class));
+            }
+            super.onStart();
+    }
+
+
 }
