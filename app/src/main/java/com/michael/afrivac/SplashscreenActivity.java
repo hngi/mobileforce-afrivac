526619321cddfa62b7d0228a3bfa008bd7fecf4f
Chris Asarhasa <jamiesplendour@gmail.com>
2020-06-22 15:51:27 +0100

    feat: Added shimmer animation to the splash screen
    wrapped the image on the SplashscreenActivity with a shimmer layout. on the SplashscreenActivity.java I set the animation to stop right before the the intent to the loginactivity or onboardscreen activity takes place

--- app/src/main/java/com/michael/afrivac/SplashscreenActivity.java
+++ app/src/main/java/com/michael/afrivac/SplashscreenActivity.java
@@ -1,11 +1,13 @@
 package com.michael.afrivac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -14,12 +16,16 @@ import com.michael.afrivac.sharePref.SharePref;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
+    ShimmerFrameLayout container;
     SharePref sharePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        container = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container1);
+        container.startShimmerAnimation();
 
         sharePref = SharePref.getINSTANCE(getApplicationContext());
 
@@ -28,9 +34,13 @@ public class SplashscreenActivity extends AppCompatActivity {
             public void run() {
 
                 if (sharePref.getIsUserLoggedIn()) {
+                    container.stopShimmerAnimation();
                     startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 } else {
+                    container.stopShimmerAnimation();
                     Intent intent = new Intent(getApplicationContext(), OnboardscreenActivity.class);
+
+
 
                     startActivity(intent);
                 }
