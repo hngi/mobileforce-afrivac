c237d372e2f4768eaadb1ff4649027e854759e84
Blexn <Onyeka_blessing@yahoo.com>
2020-06-22 17:54:25 +0100

    feat: Edit Profile Button
    Added new features to fragment_account

--- app/src/main/java/com/michael/afrivac/ui/home/HomeFragment.java
+++ app/src/main/java/com/michael/afrivac/ui/home/HomeFragment.java
@@ -8,17 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.michael.afrivac.LocationActivity;
-import com.michael.afrivac.LoginActivity;
-import com.michael.afrivac.ProfilePageActivity;
 import com.michael.afrivac.R;
 
 public class HomeFragment extends Fragment {
