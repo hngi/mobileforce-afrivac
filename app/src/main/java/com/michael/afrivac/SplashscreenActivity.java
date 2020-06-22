f8f5d9ddefde6c69321dffc68c89d69852fe9044
Chris Asarhasa <jamiesplendour@gmail.com>
2020-06-22 20:10:19 +0100

    feat: Shimmer Animation was added to the splash screen
    After the necessary implementation was added to the build.gradle, The necessary layout container was added to the activity_splashscreen.xml, also the logic was set in SplashscreenActivity.java that the animation effect would stop just when the activity intents into the login activity or the onboarding activity.

--- app/src/main/java/com/michael/afrivac/SplashscreenActivity.java
+++ app/src/main/java/com/michael/afrivac/SplashscreenActivity.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import com.michael.afrivac.sharePref.SharePref;
 
 public class SplashscreenActivity extends AppCompatActivity {
 
+    ShimmerFrameLayout container;
     SharePref sharePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        container = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container1);
+        container.startShimmerAnimation();
 
         sharePref = SharePref.getINSTANCE(getApplicationContext());
 
@@ -24,8 +29,10 @@ public class SplashscreenActivity extends AppCompatActivity {
             public void run() {
 
                 if (sharePref.getIsUserLoggedIn()) {
+                    container.stopShimmerAnimation();
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 } else {
+                    container.stopShimmerAnimation();
                     Intent intent = new Intent(getApplicationContext(), OnboardscreenActivity.class);
 
                     startActivity(intent);
