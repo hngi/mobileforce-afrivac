1cd767c3f6a5ba7b7f2149ccea75590f60ec8c3c
Umeh Michael <55640053+Michaelzy27@users.noreply.github.com>
2020-06-23 01:24:14 +0100

    Update HomeFragment.java

--- app/src/main/java/com/michael/afrivac/ui/home/HomeFragment.java
+++ app/src/main/java/com/michael/afrivac/ui/home/HomeFragment.java
@@ -8,13 +8,19 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.michael.afrivac.LocationActivity;
+import com.michael.afrivac.LoginActivity;
+import com.michael.afrivac.ProfilePageActivity;
 import com.michael.afrivac.R;
 import com.michael.afrivac.ui.account.AccountFragment;
 
