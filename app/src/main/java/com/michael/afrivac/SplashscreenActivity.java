3b4a49979bc5033e2c327eb45f4ca4e5e8c0d70b
yetundeolubola <akinwaleyetunde1@gmail.com>
2020-06-30 09:40:27 +0100

    Fixes #234

--- app/src/main/java/com/michael/afrivac/SplashscreenActivity.java
+++ app/src/main/java/com/michael/afrivac/SplashscreenActivity.java
@@ -1,16 +1,20 @@
 package com.michael.afrivac;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import com.michael.afrivac.sharePref.SharePref;
 
 public class SplashscreenActivity extends AppCompatActivity {
-    SharedPreferences onBoardPreference;
-
-
+    SharedPreferences OnBoardPreference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,19 +23,18 @@ public class SplashscreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                onBoardPreference = getSharedPreferences("onBoardScreen",MODE_PRIVATE);
-                //SharePreference which works for firstTime user to display the onBoarding Screen
-                boolean isFirstTime = onBoardPreference.getBoolean("firstTime",true);
+                //Display OnBoardScreen for firsTime User
+                OnBoardPreference = getSharedPreferences("onBoardScreen",MODE_PRIVATE);
+                boolean isFirstTime = OnBoardPreference.getBoolean("firstTime",true);
                 if(isFirstTime){
-                    SharedPreferences.Editor editor = onBoardPreference.edit();
-                    //SharePreference which works for non firstTime user to display the LoginActivity
+                    SharedPreferences.Editor editor = OnBoardPreference.edit();
                     editor.putBoolean("firstTime",false);
                     editor.apply();
                     Intent intent = new Intent(getApplicationContext(), OnboardscreenActivity.class);
                     startActivity(intent);
                     finish();
                 }else{
+                    //Display LoginActivity for non firsTime User
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                     finish();
