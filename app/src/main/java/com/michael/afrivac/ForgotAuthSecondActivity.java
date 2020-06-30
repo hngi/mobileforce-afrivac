64d787f6bf80b08dcdf0f08e8400a148896bad00
Aezikel <48924325+Aezikel@users.noreply.github.com>
2020-06-30 18:11:44 +0100

    ForgotAuth1 and 2 fix

--- app/src/main/java/com/michael/afrivac/ForgotAuthSecondActivity.java
+++ app/src/main/java/com/michael/afrivac/ForgotAuthSecondActivity.java
@@ -9,6 +9,6 @@ public class ForgotAuthSecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_auth_second);
+        setContentView(R.layout.forgot_auth_second);
     }
 }
