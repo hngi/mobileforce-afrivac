39b495a1ce507a15198e0fbcc1c33f4a90b98d49
Explicit <39551418+comfort4447@users.noreply.github.com>
2020-07-02 01:07:28 -0700

    feat:Issues #283

---
+++ FindHotelDetailsReview.java
@@ -0,0 +1,17 @@
+package com.michael.afrivac;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FindHotelDetailsReview extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_hotel_details_review);
+
+
+    }
+}
